2ed8a63b3b2b0462875aad24fff7c7cd275b0adb
Bruno <brunoyoichi@hotmail.com>
2021-06-24 09:41:17 -0400

    Delete question function

--- src/pages/Admin-Room/index.tsx
+++ src/pages/Admin-Room/index.tsx
@@ -4,6 +4,8 @@ import RoomCode from '../../components/RoomCode'
 import Button from '../../components/Button'
 import Question from '../../components/Question'
 import logoImg from '../../assets/images/logo.svg'
+import deleteImg from '../../assets/images/delete.svg'
+
 
 import './styles.scss'
 import { useAuth } from '../../hooks/useAuth'
@@ -20,6 +22,13 @@ export function AdminRoom() {
   const params = useParams<RoomParams>()
   const roomId = params.id
   const { title, questions } = useRoom(roomId)
+
+  const handleDeleteQuestion =async (questionId: string) => {
+    if (window.confirm('Tem certeza que você deseja excluir esta pergunta?')) {
+      const questionRef = await database.ref(`rooms/${roomId}/questions/${questionId}`).remove() 
+
+    }
+  }
 
   return (
     <div id="page-room">
@@ -43,7 +52,14 @@ export function AdminRoom() {
               key={question.id}
               content={question.content}
               author={question.author}
-            />
+            >
+              <button
+                type="button"
+                onClick={() => handleDeleteQuestion(question.id)}
+              >
+                <img src={deleteImg} alt="Remover pergunta" />
+              </button>
+            </Question>
           ))}
         </div>
       </main>
